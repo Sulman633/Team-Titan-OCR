be2ea5154dc552bd29caa4759e30c4ac976a266b
Biscuit93 <anthony.ferski@gmail.com>
2016-03-07 18:25:10 -0500

    all of the work

---
+++ src/test/cluster.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.util.ArrayList;
+
+public class cluster {
+	ArrayList<pixel> set;
+	int minx = Integer.MAX_VALUE;
+	int maxx = Integer.MIN_VALUE;
+	int miny = Integer.MAX_VALUE;
+	int maxy = Integer.MIN_VALUE;
+	
+	public cluster(){
+		set = new ArrayList<pixel>();
+	}
+	
+	public void addPixel(pixel p){
+		if(p.getX()>maxx) maxx=p.getX();
+		if(p.getX()<minx) minx=p.getX();
+		if(p.getY()>maxy) maxy=p.getY();
+		if(p.getY()<miny) miny=p.getY();
+		set.add(p);
+		
+	}
+	
+	public int getHeight(){
+		return maxy-miny;
+	}
+	
+	public int getWidth(){
+		return maxx-minx;
+	}
+	
+	public int getArea(){
+		return Math.abs(getHeight())*Math.abs(getWidth());
+	}
+
+}
