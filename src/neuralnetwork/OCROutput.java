29b6bc420092122ca19ebc09c1648ec9cc9e78b3
Abu <abubakar.saad.891@gmail.com>
2016-03-26 23:08:04 -0400

    List is automated now

--- src/neuralnetwork/OCROutput.java
+++ src/neuralnetwork/OCROutput.java
@@ -1,22 +1,25 @@
 package neuralnetwork;
 
+import java.util.ArrayList;
+
 public class OCROutput {
 	double[] expectedValue = null;
 	String expectedString;
 	
 	public OCROutput(String x){
-		if (x.equals("a")){
-			expectedString = "a";
-			double[] ev = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
-					1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
-			expectedValue = ev;
+		String[] array = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", ",",".","?","!"};
+		double[] ev = new double[array.length];
+		System.out.println(array.length);
+		for(int i=0; i<56; i++){
+			if(x.equals(array[i])){
+				ev[i] = 1;
+				expectedValue = ev;
+				break;
+			}
 		}
-		
-		else if (x.equals("b")){
-			
+	
+		for(int i=0; i<expectedValue.length; i++){
+			System.out.print(expectedValue[i] + ",");
 		}
 	}
-	
-	
-
 }
